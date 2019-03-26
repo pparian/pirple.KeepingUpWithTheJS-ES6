c9241f80e0893e2b4dbe4e0581763765d51c3dc8
pparian <pjp.parian@gmail.com>
2019-03-26 22:04:03 +1100

    adding event listeners for buttons

--- project1/project1.js
+++ project1/project1.js
@@ -1,10 +1,21 @@
 
-
+const generateStartButtons = () => {
+	document.getElementById("start-buttons-div").style.display = "inline";
+}
 const generateSignUpForm = () => {
 	document.getElementById("sign-up-div").style.display = "inline";
 };
 const generateLogInForm = () => {
 	document.getElementById("log-in-div").style.display = "inline";
+};
+const generateAccountSettingsForm = () => {
+	document.getElementById("account-settings-div").style.display = "inline";
+};
+const generateDashboard = async (email) => {
+	document.getElementById("dashboard-div").style.display = "inline";
+	const userData = await JSON.parse(localStorage.getItem(email));
+	document.getElementById("dashboard-header").innerText = await "Welcome to your Dashboard "
+		+ userData.firstName +"!";
 };
 const logSignUpData = (event) => {
 	event.preventDefault();
@@ -25,9 +36,15 @@ const logSignUpData = (event) => {
 	userInfo.terms = terms;
 	userInfo.lists = lists;
 
+	if (localStorage.getItem(email) === null){
+		localStorage.setItem(email,JSON.stringify(userInfo));
+		generateDashboard(email);
+	} else {
+		alert("Email already exists!");
+	}	
+
 	console.log(userInfo);
 	document.getElementById("sign-up-div").style.display = "none";
-	document.getElementById("dashboard-div").style.display = "inline";
 };
 const logLogInData = (event) => {
 	event.preventDefault();
@@ -41,20 +58,63 @@ const logLogInData = (event) => {
 	userInfo.password = password;
 
 	console.log(userInfo);
-	document.getElementById("log-in-div").style.display = "none";
-	document.getElementById("dashboard-div").style.display = "inline";
+	if (localStorage.getItem(email) != null && password === userInfo.password){
+		generateDashboard(email);
+		document.getElementById("log-in-div").style.display = "none";
+	} else {
+		alert("Email or Password does not match!");
+	}	
 };
-const cancelSignUpLogIn = (event) => {
+const logAccountSettingsData = (event) => {
+	event.preventDefault();
+	const target = event.target;
+	console.log(target)
+	let firstName = document.getElementById("firstName-accountSettings").value;
+	let lastName = document.getElementById("lastName-accountSettings").value;
+	let password = document.getElementById("password-accountSettings").value;
+	let lists = {};
+	let userInfo = {};
+
+	userInfo.firstName = firstName;
+	userInfo.lastName = lastName;
+	userInfo.password = password;
+	userInfo.lists = lists;
+
+	console.log(userInfo);
+	document.getElementById("account-settings-div").style.display = "none";
+	document.getElementById("dashboard-div").style.display = "inline";
+	// generateDashboard(email)
+};
+const cancelSignUpLogInAccountSettings = (event) => {
 	const target = event.target;
 	console.log(target)
 	if (target.id === "cancel-signUp"){
 		document.getElementById("sign-up-div").style.display = "none";
-		document.getElementById("start-buttons-div").style.display = "inline";
+		generateStartButtons();
 	} else if (target.id === "cancel-logIn"){
 		document.getElementById("log-in-div").style.display = "none";
-		document.getElementById("start-buttons-div").style.display = "inline";
+		generateStartButtons();
+	} else if (target.id === "cancel-accountSettings"){
+		document.getElementById("account-settings-div").style.display = "none";
+		document.getElementById("dashboard-div").style.display = "inline";
+		// generateDashboard(email)
 	} 
 }
+
+const dashboardDivEvent = (event) => {
+	event.preventDefault();
+	const target = event.target;
+	if (target.id === "log-out") {
+		// console.log("Clicked Log Out");
+		location.reload();
+	} else if (target.id === "account-settings") {
+		document.getElementById("dashboard-div").style.display = "none";
+		// document.getElementById("account-settings-div").style.display = "inline";
+		generateAccountSettingsForm();
+	} else if (target.id === "create-to-do-list") {
+		console.log("Clicked Create To Do List");
+	}
+};
 
 const triggerEvent = (event) => {
 	const target = event.target;
@@ -69,10 +129,22 @@ const triggerEvent = (event) => {
 	} 
 };
 
+// Event Listeners
+
+// Start Up Buttons
 document.getElementById("start-buttons-div").addEventListener("mouseup",triggerEvent);
 
+// Sign Up Form
 document.getElementById("sign-up-form").addEventListener("submit",logSignUpData);
-document.getElementById("sign-up-form").addEventListener("mouseup",cancelSignUpLogIn);
+document.getElementById("sign-up-form").addEventListener("mouseup",cancelSignUpLogInAccountSettings);
 
+// Log In Form
 document.getElementById("log-in-form").addEventListener("submit",logLogInData);
-document.getElementById("log-in-form").addEventListener("mouseup",cancelSignUpLogIn);
+document.getElementById("log-in-form").addEventListener("mouseup",cancelSignUpLogInAccountSettings);
+
+// Dashboard Buttons
+document.getElementById("dashboard-div").addEventListener("mouseup",dashboardDivEvent);
+
+// Account Settings Form
+document.getElementById("account-settings-form").addEventListener("submit",logAccountSettingsData);
+document.getElementById("account-settings-form").addEventListener("mouseup",cancelSignUpLogInAccountSettings);
